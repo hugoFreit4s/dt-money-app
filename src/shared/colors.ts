1954f00e3e7074ff54a204788fb522df2184d5f3
Hugo <hugo.encaminhados@gmail.com>
2026-05-22 11:22:00 -0300

    [Chore]: Design tokens and global styles

---
+++ src/shared/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  'accent-brand-bg-primary': '#015F43',
+  'accent-brand': '#00875F',
+  'accent-brand-light': '#00B37E',
+  'accent-red-bg-primary': '#AA2834',
+  'accent-red': '#F75A68',
+  'accent-blue': '#5A86F7',
+  'accent-blue-dark': '#284DAA',
+  'background-primary': '#121214',
+  'background-secondary': '#202024',
+  'background-tertiary': '#29292E',
+  white: '#FFFFFF',
+  gray: {
+    800: '#323238',
+    700: '#7C7C8A',
+    500: '#C4C4CC',
+  },
+};
